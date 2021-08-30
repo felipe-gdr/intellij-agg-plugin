388b3eb696aee8fa76e41bab68fd183d60a03a4e
GitHub Action <action@github.com>
2021-08-30 00:05:51 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/felipegdr/intellijaggplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.felipegdr.intellijaggplugin.services
+
+import com.github.felipegdr.intellijaggplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
